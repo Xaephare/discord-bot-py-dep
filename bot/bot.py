4d3954266dd1ce1bf122350fdaefdb6ddae57a04
Xaephare <louisgrace@onmail.com>
2022-06-16 17:39:42 +1200

    added log, scraper now works as intended

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,24 @@
 import discord
 import os
-
+import logging
 from dotenv import load_dotenv
 from discord.ext import commands
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(filename='discord_bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
 
 load_dotenv()
 token = os.getenv('TOKEN')
 
 client = discord.Client()
 
-prefix = "/"
+prfx = "/"
 
 @client.event
 async def on_ready():
@@ -20,7 +29,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(f'{prefix}greet'):
+    if message.content.startswith(f'{prfx}greet'):
         await message.channel.send('Hello! I am huge dick bot, my penis? massive.')
 
 
