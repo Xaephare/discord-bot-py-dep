27f9bbb0a82a62a06a16e4c15e8461905aac3dec
Xaephare <louisgrace@onmail.com>
2022-06-24 16:27:04 +1200

    cleaned up formatting

--- bot/bot.py
+++ bot/bot.py
@@ -11,26 +11,10 @@ handler = logging.FileHandler(filename='discord_bot.log', encoding='utf-8', mode
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
+prfx = '!'
+bot = commands.Bot(command_prefix=prfx)
 
 load_dotenv()
 token = os.getenv('TOKEN')
-
 client = discord.Client()
-
-prfx = "/"
-
-@client.event
-async def on_ready():
-    print(f"{client.user} is using enlargment pills!")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(f'{prfx}greet'):
-        await message.channel.send('Hello! I am huge dick bot, my penis? massive.')
-
-
 client.run(token)
