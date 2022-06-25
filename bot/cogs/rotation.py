3c63582952953c6d1c1fb2ad349ff6740ab983e6
Xaephare <louisgrace@onmail.com>
2022-06-25 18:22:06 +1200

    working

--- bot/cogs/rotation.py
+++ bot/cogs/rotation.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from scraper import scraper
+
 
 class Rotation(commands.Cog):
 
@@ -15,6 +17,13 @@ class Rotation(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f' {round(self.client.latency * 1000)}ms')
+
+    @commands.command()
+    async def rotation(self, ctx):
+        current = scraper()
+        await ctx.send(f'*On now:*\n`{current[0][0]}`\n*Upcoming:*\n `{current[0][1]}` in `{current[1][0]}`\n`{current[0][2]}` in `{current[1][1]}` ')
+
+        
 
 def setup(client):
     client.add_cog(Rotation(client))
--- bot/scraper.py
+++ bot/scraper.py
@@ -2,18 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-r = requests.get('https://apexlegendsstatus.com/current-map/battle_royale/pubs')
-print(r)
 
-doc = BeautifulSoup(r.text, 'html.parser')
+def scraper():
+    
+    r = requests.get('https://apexlegendsstatus.com/current-map/battle_royale/pubs')
+    #prints response code 200 is success
+    #print(r)
 
-mapList = doc.find("div", class_="row")
-mapRow = mapList.contents
+    doc = BeautifulSoup(r.text, 'html.parser')
 
-map = list((mapRow[3:6]))
-time = []
-time.append(map[1].p.text.rsplit('starts', 1)[1])
-time.append(map[2].p.text.rsplit('starts', 1)[1])
-print(map[0].h3.string)
-print(map[1].h3.string, "starts"+time[0])
-print(map[2].h3.string, "starts"+time[1])
+    mapList = doc.find("div", class_="row")
+    mapRow = mapList.contents
+
+    map = list((mapRow[3:6]))
+    time = []
+    time.append(map[1].p.text.rsplit('starts in', 1)[1])
+    time.append(map[2].p.text.rsplit('starts in', 1)[1])
+
+    current = [map[0].h3.string, map[1].h3.string, map[2].h3.string]
+    currentTime = [time[0],time[1]]
+
+
+    # print(current)
+    # print(currentTime)
+
+    return current,currentTime
