4c569510b5ed4445f9af7d2d743e5eb1239f29cd
Louis <louisgrace@onmail.com>
2022-07-05 16:25:08 +1200

    added varitions to the rotation command

--- bot/cogs/rotation.py
+++ bot/cogs/rotation.py
@@ -18,12 +18,10 @@ class Rotation(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f' {round(self.client.latency * 1000)}ms')
 
-    @commands.command()
+    @commands.command(name='Rotation', aliases=['r', 'rotation', 'R'])
     async def rotation(self, ctx):
         current = scraper()
         await ctx.send(f'*On now:*\n`{current[0][0]}`\n*Upcoming:*\n`{current[0][1]}` in `{current[1][0]}`\n`{current[0][2]}` in `{current[1][1]}` ')
-
-        
 
 def setup(client):
     client.add_cog(Rotation(client))
